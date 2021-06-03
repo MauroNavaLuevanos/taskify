143128dc7fceb69afc3525d9908b4802c62230c0
Mauro Nava Luevanos <mauronavaluevanos63@gmail.com>
2021-06-03 15:54:27 -0500

    Django jet installed
    Static root defined

--- back/back/settings.py
+++ back/back/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jet', # https://pypi.org/project/django-3-jet/
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,8 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# JET popups
+X_FRAME_OPTIONS = 'SAMEORIGIN'
