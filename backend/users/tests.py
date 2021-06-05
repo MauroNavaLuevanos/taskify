f8a3b428cff96e9edc7a5b57c24f78feeeae65ef
Mauro Nava Luevanos <mauronavaluevanos63@gmail.com>
2021-06-04 23:19:28 -0500

    Users creation test created

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,29 @@
+'''Users Tests'''
+
+# Django
 from django.test import TestCase
 
-# Create your tests here.
+# Models
+from users.models import UserModel
+
+
+class CreateUserTestCase(TestCase):
+    '''User creation test case'''
+
+    def setUp(self):
+        '''Test setup'''
+
+        pass
+
+
+    def test_code_generation(self):
+        '''User creation test genration'''
+
+        user = UserModel.objects.create(
+            first_name='fulano',
+            last_name='de tal',
+            email='fulano@spacex.com',
+            password='elpanaderoconelpan123',
+        )
+
+        self.assertIsNotNone(user)
