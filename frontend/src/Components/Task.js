867dd47d973a1684ba70f70eb698229e3ac54586
Mauro Nava Luevanos <mauronavaluevanos63@gmail.com>
2021-06-07 06:15:13 -0500

    Mark task as finished from task componetn

--- frontend/src/Components/Task.js
+++ frontend/src/Components/Task.js
@@ -1,19 +1,26 @@
 /*Task component*/
 
 // React
-import React from 'react';
+import React, { useState } from 'react';
 
 // React Router
 import { Link } from 'react-router-dom';
 
 // React Bootstrap
-import { Card, Alert, Button, Form } from 'react-bootstrap';
+import { Card, Alert, Button, Form, Spinner } from 'react-bootstrap';
+
+// Utils
+import { SendAPIRequest } from '../utils/api-calls';
 
 export default function Task(props) {
-  const task = props.task;
+  const [loading, setLoading] = useState(false);
+  const [accesToken, setAccesToken] = useState('3ff5cc5236d92852c74d7fc272b39de715061e8f');
+  const [task, setTask] = useState(props.task);
+  const [error, setError] = useState(null);
+
   let TaskCardClasses = 'Task';
 
-  if (!task.finished) {
+  if (!task.finished && !loading) {
     TaskCardClasses += ' shadow';
   } else {
     TaskCardClasses += ' bg-light';
@@ -23,20 +30,56 @@ export default function Task(props) {
     return <Alert variant="danger">Invalid task</Alert>;
   }
 
+  /**
+   * Sets a task as finished, sending a PATCH method the finished attribute as
+   * true
+   *
+   * @returns {void}
+   */
+  function SetTaskAsFinished() {
+    const taskId = task.id;
+
+    setLoading(true);
+    setError(null);
+
+    SendAPIRequest(`tasks/${taskId}/`, accesToken, { finished: true }, 'PATCH')
+      .then((response) => {
+        const { data } = response;
+
+        setTask(data);
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .then(() => {
+        setTimeout(() => {
+          setLoading(false);
+        }, 300);
+      });
+  }
+
   return (
     <div className="TaskWrapper mb-3">
       <Card className={TaskCardClasses}>
         <Card.Body>
-          <Form.Check checked={task.finished} disabled={task.finished} label="Task Completed" />
+          {loading && (
+            <Spinner animation="border" role="status" size="sm">
+              <span className="sr-only">Loading...</span>
+            </Spinner>
+          )}
+
+          <Form.Check checked={task.finished} disabled={task.finished || loading} label="Task Completed" onChange={SetTaskAsFinished} />
           <h2>{task.name}</h2>
           <p>{task.description}</p>
+          {error && <Alert>{error}</Alert>}
         </Card.Body>
+
         {!task.finished && (
           <Card.Body>
             <Link className="btn btn-primary mr-3" to={`/tasks/${task.id}/`}>
               Edit
             </Link>
-            <Button variant="danger" href="#">
+            <Button variant="danger" href="#" disabled={loading}>
               Delete
             </Button>
           </Card.Body>
