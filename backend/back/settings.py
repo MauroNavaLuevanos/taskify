0d2154d0c906940af8fa5ccde601d94d32001e44
Mauro Nava Luevanos <mauronavaluevanos63@gmail.com>
2021-06-08 06:15:55 -0500

    Using postgres db

--- backend/back/settings.py
+++ backend/back/settings.py
@@ -97,8 +97,12 @@ AUTH_USER_MODEL = 'users.UserModel'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
